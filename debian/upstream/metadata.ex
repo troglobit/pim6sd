c5daa0f3de0b8de2b317f5e3331467e6befc9c49
Marek Küthe <m.k@mk16.de>
2024-12-14 19:51:25 +0000

    Add debian package
    Signed-off-by: Marek Küthe <m.k@mk16.de>

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,7 @@
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+
+Bug-Database: https://github.com/troglobit/pim6sd/issues
+Bug-Submit: https://github.com/troglobit/pim6sd/issues/new
+Changelog: https://github.com/troglobit/pim6sd/blob/master/ChangeLog
+Repository-Browse: https://github.com/troglobit/pim6sd
+Repository: https://github.com/troglobit/pim6sd.git
